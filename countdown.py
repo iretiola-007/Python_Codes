0b4ab841d39ba5398d7fdc2629c14ba1ca17ff29
Iretiola <abisigairetiola@gmail.com>
2025-11-04 15:20:59 +0100

    Create countdown.py
    Countdown code for Python

---
+++ countdown.py
@@ -0,0 +1,19 @@
+def countdown(n):
+    if n == 0 l:
+        print("Blast off!")
+    else:
+        time.sleep(1) # 1 second delay
+        print(n)
+        countdown(n - 1)
+
+print("Welcome to Launcher Countdown!")
+
+i = str(input("Enter 'y' to continue: "))
+
+// The loop to continue using the countdown 
+while i.lower() == 'y':
+    t = int(input("Countdown from what number? "))
+    countdown(t)
+    i = str(input("Enter 'y' to continue: "))
+
+// To exit the loop, type 'n' or any other character apart from 'y'.
