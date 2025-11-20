9830a62e3629ebf23177ecf88d74f33a17b7206b
Iretiola <abisigairetiola@gmail.com>
2025-11-20 23:09:53 +0100

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choices(characters, k=length))
+    return password 
+    
+# ask the user for desired length
+length = user_input = input("Enter desired password length: ")
+
+# then convert input to integer
+try:
+    length = int(user_input)
+    print("Your new password is:", generate_password(length))
+except ValueError:
+    print("Please enter a valid number.")
+
+
